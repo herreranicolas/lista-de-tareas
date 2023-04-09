fbd2d77cd5660a7c8b5acbbe7b0fd7dbbfaa5626
Nicolas Herrera <herreranicoo17@gmail.com>
2023-04-09 20:12:44 -0300

    Cree la funcion mostrar tareas

--- js/main.js
+++ js/main.js
@@ -17,8 +17,30 @@ function agregarTarea() {
     listaDeTareas.push(tarea);
     console.log(`Se agrego la tarea ${tarea}`);
   } else {
-    alert("La tarea no puede estar vacia.")
+    alert("La tarea no puede estar vacia.");
   }
   console.log(listaDeTareas);
+  mostrarTareas();
 }
 
+function mostrarTareas() {
+  contenedorTareas.innerHTML = "";
+  listaDeTareas.map(
+    tarea =>
+      contenedorTareas.innerHTML += `
+      <article class="col">
+        <div class="card shadow-sm">
+          <div class="card-body">
+            <p class="card-text">${tarea}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-danger">Borrar</button>
+                <button type="button" class="btn btn-sm btn-warning">Editar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+  `
+  );
+}
